8b4550f4ded2f5d9618e7299c73a051edde5539a
Jay Pawar <140436138+jaypawar2004@users.noreply.github.com>
2025-02-26 23:25:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
  const userModel = require('./userModel');
-
+// 'view engine'
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
